e70a142cfb646b969ccc506bdc0672ec5df89951
MosaBapela <charitybapela2003@gmail.com>
2025-08-11 08:29:29 +0200

    Added ProtectedRoutes and Icons Components

---
+++ src/Components/Icons/EditIcon.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const EditIcon: React.FC = () => (
+  <svg xmlns="http://www.w3.org/2000/svg" className="icon" viewBox="0 0 20 20" fill="currentColor" width={20} height={20}>
+    <path d="M13.586 3.586a2 2 0 112.828 2.828l-.793.793-2.828-2.828.793-.793zm-4.606 4.606L14.393 11l-2.828 2.828-5.783-5.783 2.828-2.828z" />
+    <path
+      fillRule="evenodd"
+      d="M2 13.5V16h2.5l7.733-7.733-2.5-2.5L2 13.5z"
+      clipRule="evenodd"
+    />
+  </svg>
+);
+
+export default EditIcon;
